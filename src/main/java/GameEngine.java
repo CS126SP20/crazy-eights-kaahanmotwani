7f64b01bd1c2ff8f87f7b8b1fbaf3060c60746d9
Kaahan <kaahan15@gmail.com>
2020-02-16 15:12:12 -0600

    Created Two Player Strategies that Implement PlayerStrategy Interface

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -4,38 +4,8 @@ import student.crazyeights.PlayerTurn;
 
 import java.util.List;
 
-public class GameEngine implements PlayerStrategy {
+public class GameEngine {
 
-    public void init(int playerId, List<Integer> opponentIds) {
 
-    }
-
-    public void receiveInitialCards(List<Card> cards) {
-
-    }
-
-    public boolean shouldDrawCard(Card topPileCard, Card.Suit changedSuit) {
-        return false;
-    }
-
-    public void receiveCard(Card drawnCard) {
-
-    }
-
-    public Card playCard() {
-        return null;
-    }
-
-    public Card.Suit declareSuit() {
-        return null;
-    }
-
-    public void processOpponentActions(List<PlayerTurn> opponentActions) {
-
-    }
-
-    public void reset() {
-
-    }
 
 }
---
+++ src/main/java/student/crazyeights/PlayerStrategyTwo.java
@@ -0,0 +1,37 @@
+package student.crazyeights;
+
+import java.util.List;
+
+public class PlayerStrategyTwo implements PlayerStrategy {
+    public void init(int playerId, List<Integer> opponentIds) {
+
+    }
+
+    public void receiveInitialCards(List<Card> cards) {
+
+    }
+
+    public boolean shouldDrawCard(Card topPileCard, Card.Suit changedSuit) {
+        return false;
+    }
+
+    public void receiveCard(Card drawnCard) {
+
+    }
+
+    public Card playCard() {
+        return null;
+    }
+
+    public Card.Suit declareSuit() {
+        return null;
+    }
+
+    public void processOpponentActions(List<PlayerTurn> opponentActions) {
+
+    }
+
+    public void reset() {
+
+    }
+}
